dc0f9e4c499da5e786005fa43d53bff44f653c83
thepumpkin1979 <johan@heapsource.com>
2014-02-08 16:18:15 -0500

    [new] Tag Creation route

--- lib/app.d
+++ lib/app.d
@@ -1,5 +1,6 @@
 import webcaret.application;
 import heaploop.looping;
+import std.string : format;
 
 void main() {
     loop ^^ {
@@ -11,6 +12,14 @@ void main() {
                             <h1>Deeq API</h1>
                         </body>
                     </html>" ~ "\r\n");
+            res.end;
+        };
+
+        // curl -X POST -d "name=business" 0.0.0.0:3000/tags
+        app.router.post("/tags") ^ (req, res) {
+            auto tagName = req.form["name"];
+            res.statusCode = 201;
+            res.write("{success: true, message: \"Tag %s was created successfully\"}".format(tagName));
             res.end;
         };
         app.serve("0.0.0.0", 3000);
