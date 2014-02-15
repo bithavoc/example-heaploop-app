295407faa35c56ea151317c6e61c4f6291e9b602
thepumpkin1979 <johan@heapsource.com>
2014-02-15 09:51:17 -0500

    [new] POST /tags with couchdb persistence

--- Makefile
+++ Makefile
@@ -10,14 +10,14 @@ endif
 ifeq (${OS_NAME},Darwin)
 	DFLAGS+=-L-framework -LCoreServices 
 endif
-lib_build_params= -I../out/di ../out/webcaret.a
+lib_build_params= -I../out/di ../out/webcaret.a ../out/couched.a
 
 build: deeq-api
 
 run: cleandeps deeq-api
 	out/./deeq-api
 
-deeq-api: lib/**/*.d deps/webcaret
+deeq-api: lib/**/*.d deps/webcaret deps/couche.d
 	mkdir -p out
 	cd lib; $(DC) -of../out/deeq-api -op app.d $(lib_build_params) $(DFLAGS)
 	out/./deeq-api
@@ -35,6 +35,15 @@ deps/webcaret:
 	DEBUG=${DEBUG} $(MAKE) -C deps/webcaret
 	cp deps/webcaret/out/webcaret.a out/
 	cp -r deps/webcaret/out/di/ out/di
+
+deps/couche.d:
+	@echo "Compiling deps/couche.d"
+	git submodule update --init --remote deps/couche.d
+	mkdir -p out
+	DEBUG=${DEBUG} $(MAKE) -C deps/couche.d clean
+	DEBUG=${DEBUG} lib_build_params="-I../../../out/di ../../../out/webcaret.a" $(MAKE) -C deps/couche.d compile
+	cp deps/couche.d/out/couched.a out/
+	cp -r deps/couche.d/out/di/ out/di
 
 clean:
 	rm -rf out/*
--- lib/app.d
+++ lib/app.d
@@ -1,27 +1,68 @@
 import webcaret.application;
 import heaploop.looping;
 import std.string : format;
+import couched;
+import std.json;
 
 void main() {
     loop ^^ {
+        auto client = new CouchedClient("http://127.0.0.1:5984");
+        CouchedDatabase tags = client.databases.tags;
+        tags.ensure();
         auto app = new Application;
-        app.router.get("/") ^ (req, res) {
-            res.write(r"
-                    <html>
-                        <body>
-                            <h1>Deeq API</h1>
-                        </body>
-                    </html>" ~ "\r\n");
-            res.end;
-        };
+        with(app.router) {
+            get("/") ^ (req, res) {
+                res.write(r"
+                        <html>
+                            <body>
+                                <h1>Deeq API</h1>
+                            </body>
+                        </html>" ~ "\r\n");
+                res.end;
+            };
 
-        // curl -X POST -d "name=business" 0.0.0.0:3000/tags
-        app.router.post("/tags") ^ (req, res) {
-            auto tagName = req.form["name"];
-            res.statusCode = 201;
-            res.write("{success: true, message: \"Tag %s was created successfully\"}".format(tagName));
-            res.end;
-        };
+            // curl -X POST -d "name=business" 0.0.0.0:3000/tags
+            post("/tags") ^ (req, res) {
+                string tagName = null;
+                if("name" in req.form) {
+                    tagName = req.form["name"];
+                } else {
+                    res.statusCode = 422;
+                    res.contentType= "application/json";
+                    res.write("{success: false, message: \"Tag name is required\"}");
+                    res.end;
+                    return;
+                }
+                JSONValue existingTag;
+                try {
+                    existingTag = tags.get(tagName);
+                } catch(CouchedException cex) {
+                    if(cex.error == CouchedError.NotFound) {
+                        writeln("Tag will be created");
+                    } else {
+                        writeln("Error retrieving tag");
+                        throw cex;
+                    }
+                }
+                if(existingTag == JSONValue.init) {
+                    // create tag
+                    JSONValue tagInfo = parseJSON(q{
+                        {
+                            "name": "%s"
+                        }
+                    }.format(tagName));
+                    tags.create(tagName, tagInfo);
+                    res.statusCode = 201;
+                    res.write("{success: true, message: \"Tag %s was created successfully\"}".format(tagName));
+                    res.end;
+                    return;
+                } else {
+                    res.statusCode = 200;
+                    res.write("{success: true, message: \"Tag %s already exists\"}".format(tagName));
+                    res.end;
+                }
+            };
+        }
         app.serve("0.0.0.0", 3000);
     };
 }
