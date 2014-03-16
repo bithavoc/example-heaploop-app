0f29b64f29e80a3bd5447c03d352ef905d3aef3c
thepumpkin1979 <johan@heapsource.com>
2014-03-15 20:09:58 -0500

    [new] Welcome message :)

--- source/app.d
+++ source/app.d
@@ -5,6 +5,7 @@ import couched;
 import std.json;
 import std.process : environment;
 import std.conv : to;
+import std.stdio;
 
 void main() {
     loop ^^= {
@@ -18,27 +19,36 @@ void main() {
         auto app = new Application;
         with(app.router) {
             get("/") ^ (req, res) {
+                res.contentType = "text/html";
                 res.write(r"
                         <html>
                             <body>
-                                <h1>Deeq API</h1>
+                                <h1>Deeq JSON REST API</h1>
                             </body>
                         </html>" ~ "\r\n");
                 res.end;
             };
 
+            get("/tags") ^ (req, res) {
+                
+            };
+
             // curl -X POST -d "name=business" 0.0.0.0:3000/tags
             post("/tags") ^ (req, res) {
+                try {
                 string tagName = null;
                 if("name" in req.form) {
+                    writeln("Name param is present, cool");
                     tagName = req.form["name"];
                 } else {
+                    writeln("Tag name is required");
                     res.statusCode = 422;
                     res.contentType= "application/json";
                     res.write("{success: false, message: \"Tag name is required\"}");
                     res.end;
                     return;
                 }
+                writeln("QUERYNG DB FOR NAME" ~ tagName);
                 JSONValue existingTag;
                 try {
                     existingTag = tags.get(tagName);
@@ -58,16 +68,25 @@ void main() {
                         }
                     }.format(tagName));
                     tags.create(tagName, tagInfo);
+                    res.contentType= "application/json";
                     res.statusCode = 201;
                     res.write("{success: true, message: \"Tag %s was created successfully\"}".format(tagName));
                     res.end;
                     return;
                 } else {
                     res.statusCode = 200;
+                    res.contentType= "application/json";
                     res.write("{success: true, message: \"Tag %s already exists\"}".format(tagName));
                     res.end;
                 }
-            };
+                } catch(Throwable tex) {
+                    writeln("POST error " ~ tex.msg);
+                    res.contentType= "application/json";
+                    res.statusCode = 500;
+                    res.write("{success: false, message: \"Error occurred\"}");
+                    res.end;
+                }
+            }; // POST /tags
         }
         int port = 3000;
         string portEnv = environment.get("PORT");
